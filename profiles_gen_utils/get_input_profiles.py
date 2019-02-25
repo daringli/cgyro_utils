277f332f462ad5bfa57571d48fdeb062fc995cff
bs <nope>
2019-02-25 12:29:55 +0100

    Added utils for profiles_generation

---
+++ profiles_gen_utils/get_input_profiles.py
@@ -0,0 +1,133 @@
+from __future__ import division
+import numpy as np
+from scipy.constants import e
+
+class Input_profiles(object):
+
+
+    def read_header_data(self):        
+        findstring1 = "RADIAL GRIDPOINTS"
+        findstring2 = "N_EXP"
+        findstring3 = "IPCCW"
+        findstring4 = "BTCCW"
+        N1 = None
+        N2 = None
+        IPCCW = None
+        BTCCW = None
+        f1 = False
+        f2 = False
+        with  open(self.filename,'r') as f:
+            # first pass, we read comment lines for meta data
+            for l in f.readlines():
+                if l.find(findstring1) != -1:
+                    N1 = int(l[1:].strip().split()[3])
+                    f1= True
+                elif l.find(findstring2) != -1:
+                    N2 = int(l[1:].strip().split('=')[1])
+                    f2 = True
+                elif  l.find(findstring3) != -1:
+                    IPCCW = int(l[1:].strip().split()[2])
+                elif  l.find(findstring4) != -1:
+                    BTCCW = int(l[1:].strip().split()[2])
+                else:
+                    pass
+            else:
+                if (N1 is None) and (N2 is None):
+                    raise ValueError("File does not contain information on the number of gridpoints")
+                elif N1 is not None:
+                    N2 = N1
+                elif N2 is not None:
+                    N1 = N2
+                else:
+                   raise IllegalStateException("This should not happen") 
+                
+        self.N_radial_gridpoints = N1
+        self.IPCCW = IPCCW
+        self.BTCCW = BTCCW
+        
+    
+    def read_data(self):
+        metadata = []
+        data = {}
+        header_end_string = "rho(-)"
+        past_header = False
+        read_data = False
+        with  open(self.filename,'r') as f:
+            for l in f.readlines():
+                if not past_header:
+                    # first we search for the string signifying
+                    # that we are past header lines
+                    if l.find(header_end_string) != -1:
+                        # header found!
+                        # but we are not reading data yet
+                        past_header = True
+                if past_header:
+                    # we are past header, will only read data and metadata now
+                    if read_data:
+                        tmp = l.strip().split()
+                        if len(tmp) != len(metadata):
+                            read_data = False
+                            continue
+                        for i,d in enumerate(tmp):
+                            data[metadata[i]][iradial] = float(d)
+                        iradial = iradial + 1
+                    else:
+                        metadata = l[1:].split()
+                        for md in metadata:
+                            data[md] = np.zeros(self.N_radial_gridpoints)
+                        iradial = 0
+                        read_data = True
+        self.data = data
+                    
+
+    def __init__(self,filename):
+        self.filename = filename
+        self.read_header_data()
+        self.read_data()
+
+def get_rhoN(filename):
+    return Input_profiles(filename).data["rho(-)"]
+
+def get_q(filename):
+    return Input_profiles(filename).data["q(-)"]
+
+def get_rmaj(filename):
+    return Input_profiles(filename).data["rmaj(m)"]
+
+def get_zmag(filename):
+    return Input_profiles(filename).data["zmag(m)"]
+
+def get_kappa(filename):
+    return Input_profiles(filename).data["kappa(-)"]
+
+def get_delta(filename):
+    return Input_profiles(filename).data["delta(-)"]
+
+def get_zeta(filename):
+    return Input_profiles(filename).data["zeta(-)"]
+
+def get_r(filename):
+    return Input_profiles(filename).data["rmin(m)"]
+
+def get_ne(filename):
+    return Input_profiles(filename).data["ne(10^19/m^3)"] * 1e19
+
+def get_ni(filename,number):
+    return Input_profiles(filename).data["ni_"+str(number)+"(10^19/m^3)"] * 1e19
+
+def get_Te(filename):
+    return Input_profiles(filename).data["Te(keV)"] * 1e3 * e
+
+def get_Ti(filename,number):
+    return Input_profiles(filename).data["Ti_"+str(number)+"(keV)"] * 1e3 * e
+
+def get_psi(filename):
+    return Input_profiles(filename).data["polflux(Wb/rad)"]
+
+def get_IPCCW(filename):
+    return Input_profiles(filename).IPCCW
+    
+if __name__=="__main__":
+    IP= Input_profiles("../JET89454_2018-08-16/input.profiles")
+    print IP.N_radial_gridpoints
+    print IP.data
---
+++ profiles_gen_utils/get_input_profiles_extra.py
@@ -0,0 +1,137 @@
+from __future__ import division
+import numpy as np
+
+class Input_profiles_extra(object):
+
+    
+    def get_metadata(self):
+        with  open(self.filename,'r') as f:
+            # first pass, we read comments line for meta data
+            metadata = []
+            for l in f.readlines():
+                if l[0] == '#':
+                    _l = l[1:-1].strip()
+                    if _l != '':
+                        metadata.append(_l)
+        return metadata
+
+    def get_Nentries(self):
+        return int(self.metadata[-1].split()[-1])
+
+    @staticmethod
+    def get_index_range(intervalstring):
+        l=intervalstring.split('-')
+        if len(l) == 1:
+            return [int(l[0]),int(l[0])]
+        elif len(l) == 2:
+            return [int(l[0]),int(l[1])]
+        else:
+            raise ValueError("indexstring should be an integer 'X' or an interval on the form 'X-Y'")
+
+        
+    def get_quantity_dict(self):
+        quantity_dict = {}
+        for md in self.metadata[1:-1]:
+            mds = md.split()
+            quantity_dict[mds[1]] = Input_profiles_extra.get_index_range(mds[0])
+        return quantity_dict
+    
+    def __init__(self,filename):
+        self.filename = filename
+        self.metadata = self.get_metadata()
+        self.array_length = self.get_Nentries()
+        self.quantity_dict = self.get_quantity_dict()
+        
+
+    def get_quantity(self,quantity_name):
+        indices = self.quantity_dict[quantity_name]
+        start_location = (indices[0]-1)*self.array_length
+        stop_location = (indices[1]) *self.array_length
+        data = np.zeros(self.array_length*(indices[1]-(indices[0]-1)))
+
+        with  open(self.filename,'r') as f:
+            # second pass, we ignore comments
+            i = 0
+            for l in f.readlines():
+                if l[0] != '#':
+                    if (i>= start_location) and (i< stop_location):
+                        data[i-start_location] = float(l.strip())
+                    i = i + 1
+        return data
+
+
+def get_dVdr(filename):
+    """Function to get volp (dV/dr) from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    return IPE.get_quantity("EXPRO_volp(:)")
+
+def get_Bt0(filename):
+    """Function to get B_tor at theta=0 (T) from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    return IPE.get_quantity("EXPRO_bt0(:)")
+
+def get_Bp0(filename):
+    """Function to get B_pol at theta=0 (T) from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    return IPE.get_quantity("EXPRO_bp0(:)")
+
+def get_dlnnedr(filename):
+    """Function to get logarithmic electron density gradient from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_dlnnedr(:)")
+    return ret
+
+
+def get_dlntedr(filename):
+    """Function to get logarithmic electron temperature gradient from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_dlntedr(:)")
+    return ret
+
+def get_dlnnidr(filename):
+    """Function to get logarithmic ion density gradient from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_dlnnidr(1:10,:)")
+    Nspecies = 10
+    Nr = int(len(ret)/Nspecies)
+    return ret.reshape((Nspecies,Nr))
+
+def get_dlntidr(filename):
+    """Function to get logarithmic ion temperature gradient from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_dlntidr(1:10,:)")
+    Nspecies = 10
+    Nr = int(len(ret)/Nspecies)
+    return ret.reshape((Nspecies,Nr))
+
+def get_ni_new(filename):
+    """Function to get ion density of the first ion when QN has been enforced, from input.profiles.extra"""
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_ni_new(:)")
+    return ret
+
+def get_dlnnidr_new(filename):
+    """Function to get logarithmic ion density gradient of the first ion when QN has been enforced, from input.profiles.extra """
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_dlnnidr_new(:)")
+    return ret
+
+def get_rhos(filename):
+    IPE = Input_profiles_extra(filename)
+    ret = IPE.get_quantity("EXPRO_rhos(:)")
+    return ret
+        
+if __name__=="__main__":
+    IPE = Input_profiles_extra("../JET89454_2018-08-16/input.profiles.extra")
+    print IPE.metadata
+    print IPE.array_length
+    print IPE.quantity_dict
+    q= IPE.get_quantity("EXPRO_nuee(:)")
+    print q[0]
+    print q[-1]
+    print repr(get_dVdr("../JET89454_2018-08-16/input.profiles.extra"))
+
+    t=get_dlntidr("../JET89454_2018-08-16/input.profiles.extra")
+    print t[0]
+    #print repr(t[0])
+    #print repr(t[1])
